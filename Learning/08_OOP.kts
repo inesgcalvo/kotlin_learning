b948c5c32fc418075c768b564821b728110a0880
inesgcalvo <inglez@gmail.com>
2024-03-11 18:13:35 +0100

    chore: supermarket

--- Learning/08_OOP.kts
+++ Learning/08_OOP.kts
@@ -4,6 +4,7 @@
 // y código, en forma de procedimientos (a menudo conocidos como métodos o funciones).
 
 import java.time.LocalDate
+import kotlin.random.Random
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
@@ -257,8 +258,6 @@ object Car {
     //el motor "SM4"
     //el embrague "EM-90"
     //la caja de cambios "CH-45"
-
-
 //Las funciones del grupo propulsor de encender y apagar deben invocar las funciones del motor de encender y apagar, respectivamente.
 object Propeller {
     private val name: String = "Prop-X3"
@@ -367,3 +366,107 @@ fun driveSomewhere() {
 carInfo()
 driveSomewhere()
 carInfo()
+
+///////
+///////
+///////
+separator()
+
+// Ejercicio: modelar una caja de supermercado usando orientación a objetos
+// El objetivo de este ejercicio es poner en práctica:
+    // la representación de los datos de un objeto.
+    // la representación del comportamiento de un objeto.
+    // el paso de mensajes (o invocar funciones de un objeto desde funciones de otro objeto)
+    // la encapsulación
+    // la composición
+// Para poner en práctica la encapsulación, intenta ocultar el máximo de detalles posible desde el punto de vista del usuario del coche.
+
+// Usa la orientación a objetos para resolver el siguiente problema:
+// Nos han pedido crear el programa que se instalará en las cajas registradoras de una cadena de supermercados.
+// El programa debe permitir añadir productos con su respectivo precio a la cesta del cliente.
+// Para añadir un producto a la cesta del cliente, se debe introducir el código de barras del producto.
+// También debe permitir añadir una cierta cantidad del mismo producto.
+// El programa debe poder comunicarse con un proveedor de pagos para cargar en la tarjeta bancaria del cliente el importe final de la compra.
+// Nota: simula el comportamiento del proveedor de pagos con una lógica que simplemente diga que el pago ha ido bien.
+// El importe total antes de IVA es la suma de todos los importes de los productos de la cesta antes de aplicar el IVA correspondiente a cada producto.
+// El importe total después de IVA es la suma de todos los importes de los productos de la cesta después de aplicar el IVA correspondiente a cada producto.
+// La cadena de supermercados ya dispone de un catálogo que tiene la información de los importes y porcentajes de IVA asociados a los productos a partir de los códigos de barras.
+// Nota: puedes crear un catálogo con productos que te inventes o simplemente devolver importes y porcentajes de IVA aleatorios cada vez que se introduce un código de barras.
+// Se debe poder imprimir el ticket de la compra. El ticket debe mostrar:
+// para cada tipo de producto comprado, cantidad e importes antes y después de IVA.
+    // el importe total antes de IVA
+    // el importe total después de IVA
+
+var clientIDs: MutableSet<String> = mutableSetOf("a8e24108-b4f5-4382-9065-c76a2972cbfb", "4234b480-38de-4b24-9fb3-91fc70c4173a", "a3980899-459c-478e-a53c-8d013de8f5fc", "36a7dd9d-7316-45e5-b011-f35b1902ecf0", "32a9b4b7-c6f4-427f-bc68-27133dcc6187", "1daaf5f6-b6f1-47fe-a214-452b6536161e",   "32c9bcf1-0df5-4edd-9bdc-7c94477758d0", "b7c2e709-c25c-4b9d-a9a1-b2eb80fe2987", "ca14263d-bb40-4245-8b8c-3ecd1f4926a6", "e16ea61e-8883-4add-8d17-67e39c5ea3ff")
+var clientNames: MutableSet<String> = mutableSetOf("Raoul", "Fred", "Rita", "Misouko", "Catherine", "Ringer", "Marcia", "Moretto", "Chichin", "Valerie")
+fun generateClientID() = clientIDs.random()
+fun generateClientName() = clientNames.random()
+
+var productBarCodes: MutableSet<String> = mutableSetOf("A-0010-Z", "A-0020-Z", "A-0030-Z", "A-0040-Z", "A-0050-Z", "A-0060-Z", "A-0070-Z", "A-0080-Z", "A-0090-Z", "A-0100-Z")
+var productNames: MutableSet<String> = mutableSetOf("Tomato", "Apple", "Milk", "The", "Coffee", "Detergent", "Soap", "Rice", "Green Beans", "Orange")
+fun generateproductBarCode() = productBarCodes.random()
+fun generateProductName() = productNames.random()
+
+
+var VAT: Double = 0.21
+class SuperMarketCheckout(val clientName: String, val clientID: String) {
+    var productNameList: MutableList<String> = mutableListOf()
+    fun addProduct(product: String) {
+        productNameList.add(product)
+    }
+}
+class Client {
+    val name: String = generateClientName()
+    val clientID: String = generateClientID()
+    fun printName() {
+        println(name)
+    }
+    fun printId() {
+        println(clientID)
+    }
+}
+
+class Product {
+    val name: String = generateProductName()
+    val barcode: String = generateproductBarCode()
+    var salesPriceWithoutVAT: Double = 0.20
+    var salesPriceWithVAT: Double = salesPriceWithoutVAT * VAT
+    private var purchasePrice: Double = 0.10
+
+    fun calculateProfits () {
+        null
+    }
+    fun printName() {
+        println(name)
+    }
+    fun printBarcode() {
+        println(barcode)
+    }
+}
+
+fun infoClient() {
+    val client = Client()
+    print("Client Name: ")
+    client.printName()
+    print("Client ID: ")
+    client.printId()
+}
+
+fun infoProduct() {
+    val product = Product()
+    print("Product Name: ")
+    product.printName()
+    print("Product Barcode: ")
+    product.printBarcode()
+}
+
+fun infoSuperMarketCheckout() {
+//    println("Client ID: ${SuperMarketCheckout.clientID}")
+//    println("Client ID: ${SuperMarketCheckout.clientName}")
+//    println("Price: $${Tomato.salesPriceWithoutVAT}")
+//    println("Price + VAT: $${Tomato.salesPriceWithVAT}")
+}
+
+infoClient()
+infoProduct()
+infoSuperMarketCheckout()
